16340bde628823bb38232a9aa1801cf16881ac5c
John Mark B. Alcarde <alcardejohn3@gmail.com>
2023-01-03 18:08:51 +0800

    update

--- contact.php
+++ contact.php
@@ -3,76 +3,73 @@
  *  CONFIGURE EVERYTHING HERE
  */
 
-// // an email address that will be in the From field of the email.
-// $from = 'Demo contact form <demo@domain.com>';
+// an email address that will be in the From field of the email.
+$from = 'Demo contact form <demo@domain.com>';
 
+// an email address that will receive the email with the output of the form
+$sendTo = 'Demo contact form <demo@domain.com>';
 
-echo $_POST['email'];
+// subject of the email
+$subject = 'New message from contact form';
 
-// // an email address that will receive the email with the output of the form
-// $sendTo = 'Demo contact form <demo@domain.com>';
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
 
-// // subject of the email
-// $subject = 'New message from contact form';
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
-// // form field names and their translations.
-// // array variable name => Text to appear in the email
-// $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
-// // message that will be displayed when everything is OK :)
-// $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+/*
+ *  LET'S DO THE SENDING
+ */
 
-// // If something goes wrong, we will display this message.
-// $errorMessage = 'There was an error while submitting the form. Please try again later';
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
 
-// /*
-//  *  LET'S DO THE SENDING
-//  */
+try
+{
 
-// // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-// error_reporting(E_ALL & ~E_NOTICE);
-
-// try
-// {
-
-//     if(count($_POST) == 0) throw new \Exception('Form is empty');
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
             
-//     $emailText = "You have a new message from your contact form\n=============================\n";
+    $emailText = "You have a new message from your contact form\n=============================\n";
 
-//     foreach ($_POST as $key => $value) {
-//         // If the field exists in the $fields array, include it in the email 
-//         if (isset($fields[$key])) {
-//             $emailText .= "$fields[$key]: $value\n";
-//         }
-//     }
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
 
-//     // All the necessary headers for the email.
-//     $headers = array('Content-Type: text/plain; charset="UTF-8";',
-//         'From: ' . $from,
-//         'Reply-To: ' . $_POST['email'],
-//         'Return-Path: ' . $from,
-//     );
+    // All the necessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $_POST['email'],
+        'Return-Path: ' . $from,
+    );
     
-//     // Send email
-//     mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
-//     $responseArray = array('type' => 'success', 'message' => $okMessage);
-// }
-// catch (\Exception $e)
-// {
-//     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-// }
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
 
 
-// // if requested by AJAX request return JSON response
-// if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-//     $encoded = json_encode($responseArray);
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
 
-//     header('Content-Type: application/json');
+    header('Content-Type: application/json');
 
-//     echo $encoded;
-// }
-// // else just display the message
-// else {
-//     echo $responseArray['message'];
-// }
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
